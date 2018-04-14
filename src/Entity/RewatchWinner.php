a6e5cddf871ebe8a971775faed7043d3d00d3271
Jan Vernieuwe <jan.vernieuwe@phpro.be>
2018-04-14 22:48:15 +0200

    Record rewatch winners

---
+++ src/Entity/RewatchWinner.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RewatchWinnerRepository")
+ */
+class RewatchWinner
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $memberId;
+
+    /**
+     * @ORM\Column(type="integer", unique=true)
+     */
+    private $animeId;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $votes;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $displayName;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $episodes;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $aired;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getMemberId(): ?int
+    {
+        return $this->memberId;
+    }
+
+    /**
+     * @param int $memberId
+     * @return RewatchWinner
+     */
+    public function setMemberId(int $memberId): self
+    {
+        $this->memberId = $memberId;
+
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getAnimeId(): ?int
+    {
+        return $this->animeId;
+    }
+
+    /**
+     * @param int $animeId
+     * @return RewatchWinner
+     */
+    public function setAnimeId(int $animeId): self
+    {
+        $this->animeId = $animeId;
+
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getVotes(): ?int
+    {
+        return $this->votes;
+    }
+
+    /**
+     * @param int $votes
+     * @return RewatchWinner
+     */
+    public function setVotes(int $votes): self
+    {
+        $this->votes = $votes;
+
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getDisplayName(): ?string
+    {
+        return $this->displayName;
+    }
+
+    /**
+     * @param string $displayName
+     * @return RewatchWinner
+     */
+    public function setDisplayName(string $displayName): self
+    {
+        $this->displayName = $displayName;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param \DateTimeInterface $created
+     * @return RewatchWinner
+     */
+    public function setCreated(\DateTimeInterface $created): self
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return RewatchWinner
+     */
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEpisodes(): int
+    {
+        return $this->episodes;
+    }
+
+    /**
+     * @param mixed $episodes
+     * @return RewatchWinner
+     */
+    public function setEpisodes(int $episodes): self
+    {
+        $this->episodes = $episodes;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAired(): string
+    {
+        return $this->aired;
+    }
+
+    /**
+     * @param mixed $aired
+     * @return RewatchWinner
+     */
+    public function setAired(string $aired): self
+    {
+        $this->aired = $aired;
+
+        return $this;
+    }
+}
---
+++ src/Yasmin/Subscriber/Rewatch/RecordSubscriber.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Yasmin\Subscriber\Rewatch;
+
+use App\Channel\RewatchChannel;
+use App\Entity\RewatchWinner;
+use App\Exception\RuntimeException;
+use App\Message\RewatchNomination;
+use App\Yasmin\Event\ReactionAddedEvent;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Record a rewatch to the database
+ * Class ValidateSubscriber
+ * @package App\Yasmin\Subscriber
+ */
+class RecordSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var RewatchChannel
+     */
+    private $rewatch;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $doctrine;
+
+    /**
+     * AutoValidateSubscriber constructor.
+     * @param RewatchChannel $rewatch
+     * @param EntityManagerInterface $doctrine
+     */
+    public function __construct(RewatchChannel $rewatch, EntityManagerInterface $doctrine)
+    {
+        $this->rewatch = $rewatch;
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [ReactionAddedEvent::NAME => 'onCommand'];
+    }
+
+    /**
+     * @param ReactionAddedEvent $event
+     */
+    public function onCommand(ReactionAddedEvent $event): void
+    {
+        $reaction = $event->getReaction();
+        /** @noinspection PhpUndefinedFieldInspection */
+        if ($reaction->emoji->name !== '⏺' || !$event->isAdmin()) {
+            return;
+        }
+        if ((int)$reaction->message->channel->id !== $this->rewatch->getChannelId()) {
+            return;
+        }
+        $event->getIo()->writeln(__CLASS__.' dispatched');
+        $event->stopPropagation();
+        $io = $event->getIo();
+
+        try {
+            if (!RewatchNomination::isContender($reaction->message->content)) {
+                throw new RuntimeException('Not a contender');
+            }
+            $nomination = RewatchNomination::fromYasmin($reaction->message);
+            try {
+                $anime = $this->rewatch->getMal()->loadAnime($nomination->getAnimeId());
+            } catch (\Exception $e) {
+                throw new RuntimeException('Invalid anime link');
+            }
+        } catch (RuntimeException $e) {
+            $io->error($e->getMessage());
+
+            return;
+        }
+
+        $watch = new RewatchWinner();
+        $watch
+            ->setTitle($anime->title)
+            ->setEpisodes($anime->episodes)
+            ->setAired($anime->aired_string)
+            ->setVotes($nomination->getVotes())
+            ->setCreated(new \DateTime())
+            ->setAnimeId($anime->mal_id)
+            ->setMemberId($reaction->users->last()->id)
+            ->setDisplayName($reaction->users->last()->username);
+
+        $this->doctrine->persist($watch);
+        $this->doctrine->flush();
+        $reaction->remove($reaction->users->last());
+        $io->success(sprintf('Recorded %s', $anime->title));
+    }
+}
